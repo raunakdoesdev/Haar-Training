96f6e98e2b921bcbeb292c76f1e1d3123236205a
Sauhaarda Chowdhuri <sauhaarda@gmail.com>
2016-09-30 12:33:53 -0700

    First commit
    All tools have been tested and are working together properly.
    Test included creating a sample haar cascade from hundreds of images of boxes.
    Test output can be found in cascade2xml/boxdetector.xml

---
+++ training/haarTraining.bat
@@ -0,0 +1,3 @@
+haartraining.exe -data cascades -vec vector/facevector.vec -bg negative/bg.txt -npos 300 -nneg 200 -nstages 10 -mem 512 -mode ALL -w 80 -h 40 
+
+rem -nonsym
---
+++ training/samples_creation.bat
@@ -0,0 +1,1 @@
+createsamples.exe -info positive/info.txt -vec vector/facevector.vec -num 500 -w 80 -h 40
